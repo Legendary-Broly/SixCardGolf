22df9e73221a6871190056fdbedd1d512590c8d1
Legendary-Broly <brandonguasch@gmail.com>
2025-04-11 00:37:07 -0400

    Player score bug fix

---
+++ Assets/Scripts/PlayerCardGrid.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerCardGrid : MonoBehaviour, ICardGrid
+{
+    [SerializeField] private List<CardController> cardControllers;
+
+    public CardModel[] GetCardModels()
+    {
+        return cardControllers.Select(c => c.Model).ToArray();
+    }
+
+    public void ReplaceCard(int index, string newValue)
+    {
+        if (index < 0 || index >= cardControllers.Count) return;
+        cardControllers[index].SetCardValue(newValue);
+    }
+
+    public int FindHighestPointFaceUp()
+    {
+        return cardControllers
+            .Where(c => c.IsFaceUp)
+            .Select(c => ScoreCalculator.GetCardPointValue(c.Model.Value))
+            .DefaultIfEmpty(0)
+            .Max();
+    }
+
+    public List<string> GetFlippedCardValues()
+    {
+        return cardControllers
+            .Where(card => card.IsFaceUp)
+            .Select(card => card.Model.Value)
+            .ToList();
+    }
+
+    public List<CardController> GetCardControllers()
+    {
+        return cardControllers;
+    }
+}
